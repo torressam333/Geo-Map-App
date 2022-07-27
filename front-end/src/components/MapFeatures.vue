dd29a6f526860e95fee0f454bfdb8f5a1ad0bd43
Samuel <torressam333@gmail.com>
2022-07-27 07:42:05 -0700

    Create location search input

--- front-end/src/components/MapFeatures.vue
+++ front-end/src/components/MapFeatures.vue
@@ -2,6 +2,21 @@
   <section
     class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2] flex gap-4 px-6 py-8 md:px-0 md:py-0 bg-transparent"
   >
+    <!-- Location Searching -->
+    <div class="relative flex-1 md:min-w-[300px]">
+      <!-- search input -->
+      <input
+        aria-label="Search for location"
+        type="text"
+        class="pl-10 pr-4 py-4 text-md focus:outline-sold shadow-md rounded-md"
+        placeholder="Search for location"
+      />
+      <!-- Search icon -->
+      <div class="absolute top-0 left-[12px] h-full flex items-center">
+        <i class="fas fa-search"></i>
+      </div>
+    </div>
+    <!-- Geolocation -->
     <div
       class="px-4 bg-white flex items-center shadow-md rounded-md min-h-[50px]"
       :class="{ 'bg-slate-600': coords }"
